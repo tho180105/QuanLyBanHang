53252bbb918e99bdcc5736fda125d448ea1db777
Tho <anhjtho@gmail.com>
2022-03-29 08:37:09 +0700

    tho

---
+++ QuanLyBanHang/build/classes/GiaoDien_DangNhap/DangNhapFrame.rs
@@ -0,0 +1,8 @@
+DangNhapFrame
+DangNhapFrame$6
+DangNhapFrame$7
+DangNhapFrame$2
+DangNhapFrame$3
+DangNhapFrame$4
+DangNhapFrame$5
+DangNhapFrame$1
---
+++ QuanLyBanHang/src/GiaoDien_NhanVien/B_Nhasanxuat.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GiaoDien_NhanVien;
+
+/**
+ *
+ * @author p
+ */
+public class B_Nhasanxuat {
+    String manhasanxuat; 
+    String tennhasanxuat; 
+    String diachi; 
+    String dienthoai; 
+    String email; 
+    String mottathem; 
+    String hinh; 
+
+    public B_Nhasanxuat(String manhasanxuat, String tennhasanxuat, String diachi, String dienthoai, String email, String mottathem, String hinh) {
+        this.manhasanxuat = manhasanxuat;
+        this.tennhasanxuat = tennhasanxuat;
+        this.diachi = diachi;
+        this.dienthoai = dienthoai;
+        this.email = email;
+        this.mottathem = mottathem;
+        this.hinh = hinh;
+    }
+
+ 
+}
